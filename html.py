cffcc38ecf4981b454bd0e52876f44144c1c7b1f
Ardent001 <105624195+Ardent001@users.noreply.github.com>
2022-05-17 18:15:26 +0100

    Add files via upload

---
+++ html.py
@@ -0,0 +1,11 @@
+from cgitb import html
+import request as kind
+
+url_input = input('Webpage to grab source from: ')
+url_output = input('Name for html file: ')
+
+req = kind.get(url, 'html.parser')
+
+with open(url_output, 'w') as url:
+    url.write(req.text)
+    url.close()
